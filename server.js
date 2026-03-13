4f79f52cd414da48e9f51afbd6c82b7cde1b5e95
Ravishankar Tiwari <tiwariravishankar1208@gmail.com>
2026-03-13 19:28:48 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();   // server created
+
+app.get('/',(req, res) =>{
+  res.send("hello world")
+})
+
+
+app.get ('/about',function(req, res) {
+  res.send("this is about page")
+})
+
+
+app.get ('/home',function(req, res) {
+  res.send("this is home page")
+})
+
+
+
+app.listen(3000);
